39927406edd02e54833ec56d4e63b5e2d86c76e4
Shamima Ali <snali@mun.ca>
2022-05-16 17:12:20 -0230

    cleaning up

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import './App.css'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import bootstrap from 'bootstrap'
-import Typist from "react-typist";
 import Header from './components/Header/Header';
 import Footer  from './components/Footer/Footer';
 import Description from './components/Description/Description';
 import Work from './components/Work/Work';
-import {Row,Col, Container} from 'react-bootstrap'
 
 
 function App() {
